1392df8dfa6107f32684ee459cccad5c7327df5d
fahimreza-dev <fahim@plugpress.co>
2024-06-01 19:17:43 +0600

    Initialization

--- webpack.config.js
+++ webpack.config.js
@@ -17,12 +17,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        use: 'babel-loader',
       },
       {
         test: /\.scss$/,
